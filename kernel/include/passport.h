384a94e1e7a24870939cc8073a842be4064037c7
xcmsx <164064657@qq.com>
2026-07-25 20:45:52 +0800

    Update passport.h

--- kernel/include/passport.h
+++ kernel/include/passport.h
@@ -1,11 +1,8 @@
-// kernel/include/passport.h
 #ifndef _KPASSPORT_H_
 #define _KPASSPORT_H_
 
-#include <linux/sched.h>   // 提供 struct task_struct
-
 void init_passport(void);
-int has_passport(struct task_struct *task);
+int has_passport(void);          // 改为无参，直接使用 current
 int remove_passport_pid(pid_t pid);
 void clear_passport(void);
 
