ea614238e92f21f5058eebe86bdaf299301d34b0
xcmsx <164064657@qq.com>
2026-07-25 20:51:10 +0800

    Update passport.h

--- kernel/include/passport.h
+++ kernel/include/passport.h
@@ -1,8 +1,10 @@
 #ifndef _KPASSPORT_H_
 #define _KPASSPORT_H_
 
+#include <linux/types.h>   // 提供 pid_t
+
 void init_passport(void);
-int has_passport(void);          // 改为无参，直接使用 current
+int has_passport(void);            // 不再传参，直接使用 current
 int remove_passport_pid(pid_t pid);
 void clear_passport(void);
 
